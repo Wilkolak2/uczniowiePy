ae87b184fc6abf0a738010237121efc7e64c427d
Kinga Smolarek <100038018+Radio2840@users.noreply.github.com>
2024-12-02 14:13:21 +0100

    WOW2

--- main.py
+++ main.py
@@ -10,9 +10,23 @@ class MyForm(QDialog):
         self.ui = Ui_Dialog()
         super().__init__()
         self.ui.setupUi(self)
+        # self.ui.students.itemSelectionChanged.connect(self.students_fail)
+        self.ui.augustButton.clicked.connect(self.august)
         self.show()
 
 
+    def august(self):
+        items = self.ui.students.selectedItems()
+        for item in items:
+            self.ui.failedStudents.addItem(item.text())
+            self.ui.students.takeItem(self.ui.students.row(item))
+"""
+    def students_fail(self):
+        items = self.ui.students.selectedItems()
+        self.ui.students.removeItemWidget(items[0])
+        for item in items:
+            self.ui.failedStudents.addItem(item.text())
+"""
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     ex = MyForm()
